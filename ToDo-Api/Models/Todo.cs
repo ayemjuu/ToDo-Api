ff34b44a9f24550bf17336b80b11f2fd713c0ea4
Kyle <kaijuuuuu0@gmail.com>
2024-06-23 22:32:21 +0800

    Add project files.

---
+++ ToDo-Api/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
